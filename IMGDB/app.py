426d98cab827599f725660296e1cf050b125efa5
MidovG <139575579+MidovG@users.noreply.github.com>
2023-07-14 19:57:15 +0300

    Add files via upload

---
+++ IMGDB/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, Response
+from werkzeug.utils import secure_filename
+import os
+from db import db_init, db
+from modeles import Img
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db_init(app)
+
+def root_dir(): 
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_file(filename):
+    try:
+        src = os.path.join(root_dir(), filename)
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+@app.route('/')
+def hello(): 
+    content = get_file('index.html')
+    return Response(content, mimetype="text/html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    if not pic:
+        return 'No pic uploaded!', 400
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return 'Bad upload!', 400
+
+    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+    db.session.add(img)
+    db.session.commit()
+
+    return 'Img Successfull Uploaded!', 200
+
+@app.route('/<int:id>')
+def get_img(id):
+    img = Img.query.filter_by(id=id).first()
+    if not img:
+        return 'Img Not Found!', 404
+
+    return Response(img.img, mimetype=img.mimetype)
